5f3246f33c8d784da8bd9b018b70461b305fad4d
rahulbpc <rahul.balgotra@technocratshorizons.com>
2025-07-31 15:54:12 +0530

    Fix: renamed 'Front' to 'front' and updated homepage text

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -69,7 +69,7 @@
 
 <footer class="bg-dark py-3 bg-2">
 <div class="container">
-    <p class="text-center text-white pt-3 fw-bold fs-6">© 2023 xyz company, all right reserved</p>
+    <p class="text-center text-white pt-3 fw-bold fs-6">© 2023 job portal company, all right reserved</p>
 </div>
 </footer> 
 <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
